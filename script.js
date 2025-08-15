2ab87dc4d8c227ce60cd1efc1b1acc48c3ab9648
Giovanna <gi.cinoca@hotmail.com>
2025-08-15 08:43:29 -0300

    refactor: refactor API fetching to use async/await

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos")
-.then(res => (res.json()))
-.then((videos) => 
-    videos.forEach((video) => {
-        containerVideos.innerHTML += `
-        <li class="videos__item">
-            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe> 
-            <div class="descricao-video">
-                <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
-                <h3 class="titulo-video">${video.titulo}</h3>
-                <p class="titulo-canal">${video.descricao}</p>
-            </div>
-        </li>
-        `;
-    })
-)
-.catch((error) => {
-    containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error} </p>`;
+async function buscarEMostrarVideos(params) {
+    try {
+        const busca =  await fetch("http://localhost:3000/videos");
+        const videos = await busca.json();
+
+        videos.forEach((video) => {
+            containerVideos.innerHTML += `
+            <li class="videos__item">
+                <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe> 
+                <div class="descricao-video">
+                    <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
+                    <h3 class="titulo-video">${video.titulo}</h3>
+                    <p class="titulo-canal">${video.descricao}</p>
+                </div>
+            </li>
+            `;
+        })
+    } catch(error) {
+        containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error}</p>`
+    } finally {
+        console.log('Tentativa de carregar vídeos finalizada.');
+    }
 }
-)
+
+buscarEMostrarVideos();
